c974e743cb3b001d1dbd9ffa2dc8ff97730bb2cc
jsdealy <dealyjustins@gmail.com>
2024-10-25 12:28:44 -0400

    random chooser added

--- modz/randomChooser.py
+++ modz/randomChooser.py
@@ -3,4 +3,4 @@ from .botsay import botsay
 
 async def randomChooser(mess: str, channel):
     alternatives = mess.removeprefix("choose:").strip().split(";")
-    await botsay(f"I choose {alternatives[randint(0,len(alternatives)-1)]}",channel)
+    await botsay(f"I choose {alternatives[randint(0,len(alternatives)-1)].strip()}! :pregnant_man:",channel)
