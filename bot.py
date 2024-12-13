9d6ba7cd38b39eed7fd4aa1d8e8b914e6001739b
jsdealy <dealyjustins@gmail.com>
2024-12-13 11:40:48 -0500

    hm

--- bot.py
+++ bot.py
@@ -263,14 +263,16 @@ async def grab(interaction: discord.Interaction,genre: str,language: str,visibil
         else:
             cannes_films = select(con.cur(),"Films.title","Films.tconst",tables=["Cannes","Films","Genres","Ratings"],joins=["Ratings.tconst=Films.tconst","Films.tconst=Cannes.tconst","Cannes.tconst=Genres.tconst"],qualifiers=[f"AND Ratings.rating > 6.5 AND Ratings.numVotes > 3000 ORDER BY RANDOM() LIMIT {CANNES_LIMIT}"],Genres__genre=genre)
             gen_films = select(con.cur(),"Films.title","Films.tconst",tables=["Films","Genres","Ratings"],joins=["Ratings.tconst=Films.tconst","Films.tconst=Genres.tconst"],qualifiers=[f"AND Ratings.rating > 6.5 AND Ratings.numVotes > 3000 ORDER BY RANDOM() LIMIT {5-len(cannes_films)}"],Genres__genre=genre)
-        res = f"{'\n'.join([f"[{x[0]}](http://www.imdb.com/title/{x[1]})" for x in set(cannes_films + gen_films)])}"
+        res = ""
+        if len(cannes_films) > 0 or len(gen_films) > 0:
+            res = f"{'\n'.join([f"[{x[0]}](http://www.imdb.com/title/{x[1]})" for x in set(cannes_films + gen_films)])}"
         print(f"res:>{res}<")
         print(len(res))
-        res = res if len(res.strip('\n').strip())>0 else "No results! :pregnant_man:"
+        res = res if len(res)>0 else "No results! :pregnant_man:"
         print(f"res:>{res}<")
         print(len(res))
-        await interaction.response.send_message("test",ephemeral=True if visibility == 0 else False)
-        # await interaction.response.send_message(res,ephemeral=True if visibility == 0 else False)
+        # await interaction.response.send_message("test",ephemeral=True if visibility == 0 else False)
+        await interaction.response.send_message(res,ephemeral=True if visibility == 0 else False)
     except Exception as e:
         try:
             await interaction.response.send_message(f"Error: {e}")
