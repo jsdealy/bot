932b8c01bd75d4b6536f9bdbe631c92e1a256eee
jsdealy <dealyjustins@gmail.com>
2024-12-13 11:23:29 -0500

    hm

--- bot.py
+++ bot.py
@@ -266,7 +266,7 @@ async def grab(interaction: discord.Interaction,genre: str,language: str,visibil
         res = f"{'\n'.join([f"[{x[0]}](http://www.imdb.com/title/{x[1]})" for x in set(cannes_films + gen_films)])}"
         print(f"res:>{res}<")
         res = res if len(res.strip())>0 else "No results! :pregnant_man:"
-        print(res)
+        print(len(res.strip())>0)
         await interaction.response.send_message(res,ephemeral=True if visibility == 0 else False)
     except Exception as e:
         try:
